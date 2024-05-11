5e63fc610823731573ef05aeeb6142bdf1cea980
serhatbalik <mserhatbalik@gmail.com>
2024-05-11 20:16:23 +0300

    Created basic template and home page routing

--- foodie/views.py
+++ foodie/views.py
@@ -1,4 +1,6 @@
-from django.http import HttpResponse
+# render home.html template
+from django.shortcuts import render
+
 
 def home(request):
-    return HttpResponse("Hello, World!")
+    return render(request, 'home.html')
