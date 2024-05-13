ea940557dcb73ab5df980e114c783cd8dec4bf66
serhatbalik <mserhatbalik@gmail.com>
2024-05-13 21:00:42 +0300

    Created custom user model. Next is migrations.

--- foodie/settings.py
+++ foodie/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "accounts.apps.AccountsConfig"
 ]
 
 MIDDLEWARE = [
@@ -85,6 +86,8 @@ DATABASES = {
     }
 }
 
+# Custom user model
+AUTH_USER_MODEL = 'accounts.User'
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
