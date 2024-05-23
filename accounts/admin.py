f2e7c3fd70d8ce695c283c4a1489845147aed94b
serhatbalik <mserhatbalik@gmail.com>
2024-05-23 20:53:21 +0300

    Included Custom User model in the Admin.py and modified the fields in the LIST view of Users in the Admin panel.

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+
 
 # Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    filter_horizontal = ()
+    list_filter = ()
+
+    ordering = ('-date_joined',)
+
+    # CHANGE THE FIELDS IN THE "LIST" PAGE.
+    list_display = ('email', 'first_name', 'last_name', 'username', 'role', 'is_active')
+
+    # CHANGE THE FIELDS IN THE "ADD and MODIFY" PAGES.
+    # fieldsets = (
+    #     ('Meta Information', {'fields': ('username', 'email')}),
+    #     ('Personal Information', {'fields': (('first_name', 'last_name'), 'phone_number', 'role')}),
+    # )
+
+
+admin.site.register(User, CustomUserAdmin)
