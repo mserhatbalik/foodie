04ec943efbf54490594d4984df69053f2298e1c3
serhatbalik <mserhatbalik@gmail.com>
2024-05-24 16:17:07 +0300

    Created UserProfile model and included in Admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import User, UserProfile
 
 
 # Register your models here.
@@ -22,3 +22,4 @@ class CustomUserAdmin(UserAdmin):
 
 
 admin.site.register(User, CustomUserAdmin)
+admin.site.register(UserProfile)
